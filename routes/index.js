9daf11938161650443e443ee9243df1b463394f9
Teisia Park <teisia@gmail.com>
2016-01-11 16:13:20 -0700

    bump heroku

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+
 var knex = require('knex')({
   client: 'pg',
   connection: process.env.DATABASE_URL || 'postgres://localhost/eat'
