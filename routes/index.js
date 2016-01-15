9bcf95e9d5bd962a2f69dbb03ec118abf3facdce
Teisia Park <teisia@gmail.com>
2016-01-15 10:31:08 -0700

    added database url to .env

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
-var knex = require('../db/knex')({
-  function restaurants() {
-    return knex('restaurants');
-  }
-  function employees() {
+function restaurants() {
+  return knex('restaurants');
+};
+
+function employees() {
     return knex('employees');
-  }
-});
+};
 
 router.get('/', function(req, res, next) {
     res.redirect('/restaurants');
