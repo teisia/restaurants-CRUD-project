eeea435497ee9f5083ca8c8e815c517ff26d7d21
Teisia Park <teisia@gmail.com>
2016-01-20 16:28:32 -0700

    hardcoded denver as first map

--- routes/neighborhoods.js
+++ routes/neighborhoods.js
@@ -5,13 +5,13 @@ var request = require('request');
 
 // show neighborhoods page
 router.get('/', function(req, res, next) {
-  var google_api = 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA';
+  var google_api = 'https://maps.googleapis.com/maps/api/geocode/json?address=1430+Larimer+St,+Denver,+CO';
   var my_key = '&key='+'AIzaSyC_AEJoor25uoZy70X3iaMELWOJe14n8HE';
     request(google_api+my_key, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var jase = JSON.parse(body);
         var lat_long = jase.results[0].geometry.location;
-          res.render('pages/neighborhoods', { lat_long: lat_long});
+          res.render('pages/neighborhoods', {lat_long: lat_long});
     }
   })
 });
