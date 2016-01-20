f9a3bf3d785773a383adfde5a02cb7f960c7cb29
Teisia Park <teisia@gmail.com>
2016-01-20 16:22:22 -0700

    google maps api working with class example

--- routes/neighborhoods.js
+++ routes/neighborhoods.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+var request = require('request');
 
-
-// show restaurants homepage
-router.get('/', function(req, res) {
-  res.render('pages/neighborhoods');
+// show neighborhoods page
+router.get('/', function(req, res, next) {
+  var google_api = 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA';
+  var my_key = '&key='+'AIzaSyC_AEJoor25uoZy70X3iaMELWOJe14n8HE';
+    request(google_api+my_key, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var jase = JSON.parse(body);
+        var lat_long = jase.results[0].geometry.location;
+          res.render('pages/neighborhoods', { lat_long: lat_long});
+    }
+  })
 });
 
 module.exports = router;
