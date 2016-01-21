cbe3c9248502a2d96bb445720ba7e83cc3bccb8b
Teisia Park <teisia@gmail.com>
2016-01-21 11:55:32 -0700

    added new route and form

--- routes/neighborhoods.js
+++ routes/neighborhoods.js
@@ -9,33 +9,43 @@ function neighborhoods() {
 
 // show neighborhoods page
 router.get('/', function(req, res, next) {
-  var google_api = 'https://maps.googleapis.com/maps/api/geocode/json?address=1430+Larimer+St,+Denver,+CO';
-  var my_key = '&key='+'AIzaSyC_AEJoor25uoZy70X3iaMELWOJe14n8HE';
-    request(google_api+my_key, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var jase = JSON.parse(body);
-        var lat_long = jase.results[0].geometry.location;
-          res.render('pages/neighborhoods', {lat_long: lat_long});
-    }
+  neighborhoods().first().then(function(result) {
+    var google_api = 'https://maps.googleapis.com/maps/api/geocode/json';
+    var address = '?address='+epicenter+'';
+    var epicenter = result.epicenter.split(' ').join('+');
+    var my_key = '&key='+'AIzaSyC_AEJoor25uoZy70X3iaMELWOJe14n8HE';
+      request(google_api+address+my_key, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var jase = JSON.parse(body);
+          var lat_long = jase.results[0].geometry.location;
+            res.render('pages/neighborhoods', {lat_long: lat_long, neighborhoods: result});
+      }
+    })
   })
+});
+
+// show add new neighborhood page
+router.get('/new', function(req, res, next) {
+  res.render('pages/neighborhood-new');
 });
 
 // show individual neighborhood page
 router.get('/:id', function(req, res, next) {
   neighborhoods().where('id', req.params.id).first().then(function(result) {
-    var address = result.epicenter;
-    address.replace(/ /g,'+');
+    var address = result.epicenter.split(' ').join('+');
     var google_api = 'https://maps.googleapis.com/maps/api/geocode/json?address='+address+'';
     var my_key = '&key='+'AIzaSyC_AEJoor25uoZy70X3iaMELWOJe14n8HE';
     request(google_api+my_key, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var jase = JSON.parse(body);
-        var neighborhoodName = jase.results[0].address_components[2].long_name;
+        var city = jase.results[0].address_components[3].long_name;
+        var state = jase.results[0].address_components[5].long_name;
         var lat_long = jase.results[0].geometry.location;
-          res.render('pages/neighborhood-indv', {lat_long: lat_long, neigborhoods: result});
+          res.render('pages/neighborhood-indv', {lat_long: lat_long, neighborhoods: result});
       }
     })
   })
-})
+});
+
 
 module.exports = router;
