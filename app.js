3d092400166754dbb6fbbe762136829b72375a47
Teisia Park <teisia@gmail.com>
2016-01-26 16:19:52 -0700

    changed admin page to login page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 require('dotenv').load();
+var authChecker = require('./lib/authChecker')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
